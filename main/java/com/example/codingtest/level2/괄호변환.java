3fda7a23a4aab1c37a4f5daab44a0bd5b90a64c4
Taehee Lim <lim3617@naver.com>
2022-02-03 17:26:48 +0900

    hard

--- main/java/com/example/codingtest/level2/괄호변환.java
+++ main/java/com/example/codingtest/level2/괄호변환.java
@@ -1,5 +1,8 @@
 package com.example.codingtest.level2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class 괄호변환 {
@@ -8,25 +11,57 @@ public class 괄호변환 {
         String data2 = ")(";
         String data3 = "()))((()";
 
-        System.out.println(solution(data1));;
+        System.out.println(solution(data3));
     }
     // ( = 0
     // ) = 1
     // 01 0101 0011 0 0 01 1 01 1
     public static String solution(String p){
         System.out.println(p);
-        String a  = "(";
-        String b = ")";
+        String a  = "("; // push
+        String b = ")"; // pop
+        String[] split = p.split("");
+        System.out.println(Arrays.toString(split));
 
         if(p.equals("")){
             return "";
         }
-        Stack<String> stack = new Stack<>();
-        for(int i = 0; i < p.length(); i++){
-            stack.push(String.valueOf(p.charAt(i)));
-        }
 
-        
+        Stack<String> all = new Stack<>();
+        List<String> list = new ArrayList<>();
+
+        String u = "";
+        String v = "";
+        int first = 0;
+        int second = 0;
+
+        Loop1:
+        for(int i = 0; i < split.length; i++){
+            if(split[i].equals("(")){
+                first++;
+                System.out.println("first");
+                System.out.println(first);
+                list.add(split[i]);
+            } else {
+                second++;
+                System.out.println("second");
+                System.out.println(second);
+                list.add(split[i]);
+            }
+
+            if(first == second){
+                System.out.println(first);
+                System.out.println(second);
+                for(int j = 0; j < list.size(); j++){
+                    u += list.get(j);
+                    if(j == list.size() - 1){
+                        break Loop1;
+                    }
+                }
+            }
+        }
+        System.out.println(u);
+
 
         return "";
     }
