44b52d6b6488edcd31e0ed1febf7120bdd55e67b
Taehee Lim <lim3617@naver.com>
2022-02-06 09:47:34 +0900

    in PROGRESS

---
+++ main/java/com/example/codingtest/level1/신고결과.java
@@ -0,0 +1,20 @@
+package com.example.codingtest.level1;
+
+public class 신고결과 {
+    public static void main(String[] args) {
+        String[] idList = {"muzi", "frodo", "apeach", "neo"};
+        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+        int k = 2;
+
+        String[] idList2 = {"con", "ryan"};
+        String[] report2 = {"ryan con", "ryan con", "ryan con", "ryan con"};
+        int k2 = 3;
+    }
+
+    public static int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+
+
+        return answer;
+    }
+}
