5cb5e6679e27ee53e7228ec40840ae0b0b95aaba
Taehee Lim <lim3617@naver.com>
2022-02-08 15:19:54 +0900

    in progress : 비밀지도

---
+++ main/java/com/example/codingtest/level1/비밀지도.java
@@ -0,0 +1,23 @@
+package com.example.codingtest.level1;
+
+import java.util.Arrays;
+
+public class 비밀지도 {
+    public static void main(String[] args) {
+        int n1 = 5;
+        int[] arr1 = {9, 20, 28, 18, 11};
+        int[] arr2 = {30, 1, 21, 17, 28};
+
+        int n2 = 6;
+        int[] arr12 = {46, 33, 33 ,22, 31, 50};
+        int[] arr22 = {27 ,56, 19, 14, 14, 10};
+
+        System.out.println(Arrays.toString(solution(n1, arr1, arr2)));
+    }
+
+    public static String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+
+        return answer;
+    }
+}
