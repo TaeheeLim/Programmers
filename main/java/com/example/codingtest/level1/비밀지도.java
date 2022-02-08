94db7b75ea5ac8af4a93a1a6ffccb204ea11eb0a
Taehee Lim <lim3617@naver.com>
2022-02-08 16:23:00 +0900

    done: 비밀지도

--- main/java/com/example/codingtest/level1/비밀지도.java
+++ main/java/com/example/codingtest/level1/비밀지도.java
@@ -17,7 +17,35 @@ public class 비밀지도 {
 
     public static String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = {};
+        answer = new String[n];
 
+        for(int i = 0; i < arr1.length; i++){
+            String firstBinaryNumber = Integer.toBinaryString(arr1[i]);
+            if(firstBinaryNumber.length() < n){
+                int goRound = n - firstBinaryNumber.length();
+                for(int j = 0; j < goRound; j++){
+                    firstBinaryNumber = "0" + firstBinaryNumber;
+                }
+            }
+            String secondBinaryNumber = Integer.toBinaryString(arr2[i]);
+            if(secondBinaryNumber.length() < n){
+                int goRound = n - secondBinaryNumber.length();
+                for(int j = 0; j < goRound; j++){
+                    secondBinaryNumber = "0" + secondBinaryNumber;
+                }
+            }
+            StringBuilder builder = new StringBuilder();
+            for(int k = 0; k < n; k++){
+                if(firstBinaryNumber.charAt(k) != secondBinaryNumber.charAt(k)){
+                    builder.append("#");
+                } else if(firstBinaryNumber.charAt(k) == '0'){
+                    builder.append(" ");
+                } else if(firstBinaryNumber.charAt(k) == '1'){
+                    builder.append("#");
+                }
+            }
+            answer[i] = builder.toString();
+        }
         return answer;
     }
 }
