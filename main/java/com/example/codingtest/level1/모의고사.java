919694dddc4221252e306e1ebc4c86511ce2c1a6
Taehee Lim <lim3617@naver.com>
2022-02-08 11:11:15 +0900

    In progress : 모의고사

---
+++ main/java/com/example/codingtest/level1/모의고사.java
@@ -0,0 +1,19 @@
+package com.example.codingtest.level1;
+
+import java.util.Arrays;
+
+public class 모의고사 {
+    public static void main(String[] args) {
+        int[] answers1 = {1,2,3,4,5};
+        int[] answers2 = {1,3,2,4,2};
+
+        System.out.println(Arrays.toString(answers1));
+    }
+
+    public static int[] solution(int[] answers){
+        int[] answer = {};
+
+        
+        return answer;
+    }
+}
