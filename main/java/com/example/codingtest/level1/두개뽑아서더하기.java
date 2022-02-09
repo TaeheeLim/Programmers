d5bd44e21760db278183f79695718816a35d70fb
Taehee Lim <lim3617@naver.com>
2022-02-09 11:56:04 +0900

    Add : 음양더하기

--- main/java/com/example/codingtest/level1/두개뽑아서더하기.java
+++ main/java/com/example/codingtest/level1/두개뽑아서더하기.java
@@ -1,4 +1,13 @@
 package com.example.codingtest.level1;
 
 public class 두개뽑아서더하기 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] solution(int[] numbers) {
+        int[] answer = {};
+        
+        return answer;
+    }
 }
--- main/java/com/example/codingtest/level1/음양더하기.java
+++ main/java/com/example/codingtest/level1/음양더하기.java
@@ -1,4 +1,23 @@
 package com.example.codingtest.level1;
 
 public class 음양더하기 {
+    public static void main(String[] args) {
+        int[] absolutes1 = {4,7,12};
+        boolean[] signs1 = {true,false,true};
+
+        int[] absolutes2 = {1,2,3};
+        boolean[] signs2 = {false,false,true};
+
+        System.out.println(solution(absolutes2, signs2));
+    }
+
+    public static int solution(int[] absolutes, boolean[] signs) {
+        int sum = 0;
+        for(int i = 0; i < absolutes.length; i++){
+            if(signs[i]) sum += absolutes[i];
+            else sum -= absolutes[i];
+        }
+        return sum;
+    }
+
 }
