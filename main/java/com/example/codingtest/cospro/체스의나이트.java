a641c303bb8821de4b061e13f3646c1b8f911ddd
Taehee Lim <lim3617@naver.com>
2022-02-15 10:24:35 +0900

    in progress

---
+++ main/java/com/example/codingtest/cospro/체스의나이트.java
@@ -0,0 +1,20 @@
+package com.example.codingtest.cospro;
+
+public class 체스의나이트 {
+    public static void main(String[] args) {
+        String data1 = "A7";
+        String data2 = "A5";
+        String data3 = "D4";
+        String data4 = "A1";
+        String data5 = "H1";
+        String data6 = "H8";
+
+        System.out.println(solution(data1));
+    }
+
+    public static int solution(String pos) {
+        int answer = 0;
+        
+        return answer;
+    }
+}
--- main/java/com/example/codingtest/level2/거리두기확인하기.java
+++ main/java/com/example/codingtest/level2/거리두기확인하기.java
@@ -1,4 +1,4 @@
-package com.example.codingtest.level1;
+package com.example.codingtest.level2;
 
 import java.util.Arrays;
 import java.util.LinkedList;
