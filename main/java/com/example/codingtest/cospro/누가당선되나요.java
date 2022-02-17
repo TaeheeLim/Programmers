1ff8efdb5235bed82042b10dd4d588cdf8652e6d
Taehee Lim <lim3617@naver.com>
2022-02-17 14:09:38 +0900

    done : 병합and정렬 etc

--- main/java/com/example/codingtest/cospro/누가당선되나요.java
+++ main/java/com/example/codingtest/cospro/누가당선되나요.java
@@ -13,15 +13,21 @@ public class 누가당선되나요 {
         int N2 = 4;
         int[] votes2 = {1, 3, 2, 3, 2};
         int[] ret2 = solution(N2, votes2);
+
+        System.out.println(Arrays.toString(ret2));
     }
 
     public static int[] solution(int N, int[] votes){
-        int voteCounter[] = new int[11];
+        int[] voteCounter = new int[11];
+
+        //후보 번호를 인덱스로 보고 해당 배열 index를 ++
         for (int i = 0; i < votes.length; i++) {
             voteCounter[votes[i]] += 1;
         }
+        System.out.println(Arrays.toString(voteCounter));
         int maxVal = 0;
         int cnt = 0;
+
         for (int i = 1; i <= N; i++) {
             if (maxVal < voteCounter[i]) {
                 maxVal = voteCounter[i];
@@ -31,13 +37,16 @@ public class 누가당선되나요 {
                 cnt += 1;
             }
         }
+
         int answer[] = new int[cnt];
+
         for (int i = 1, idx = 0; i <= N; i++){
             if (voteCounter[i] == maxVal) {
-                answer[idx] = voteCounter[i];
+                answer[idx] = i;
                 idx += 1;
             }
         }
+
         return answer;
     }
 }
---
+++ main/java/com/example/codingtest/cospro/병합and정렬.java
@@ -0,0 +1,45 @@
+package com.example.codingtest.cospro;
+
+import java.util.Arrays;
+
+public class 병합and정렬 {
+    public static void main(String[] args) {
+        int[] arrA = {-2, 3, 5, 9};
+        int[] arrB = {0, 1, 5};
+        int[] ret = solution(arrA, arrB);
+
+        System.out.println(Arrays.toString(ret));
+    }
+
+    public static int[] solution(int[] arrA, int[] arrB){
+        int arrA_idx = 0, arrB_idx = 0;
+
+        int arrA_len = arrA.length;
+        int arrB_len = arrB.length;
+
+        int[] answer = new int[arrA_len + arrB_len];
+
+        int answer_idx = 0;
+
+//        int[] arrA = {-2, 3, 5, 9};
+//        int[] arrB = {0, 1, 5};
+//answer  -2 0 1 3 5
+//arrA_idx 0 1 1 2 2
+//arrB_idx 0 1 2 2 3
+        while(arrA_idx < arrA_len && arrB_idx < arrB_len){
+            if(arrA[arrA_idx] < arrB[arrB_idx])
+                answer[answer_idx++] = arrA[arrA_idx++];
+            else
+                answer[answer_idx++] = arrB[arrB_idx++];
+        }
+
+        while(arrA_len - arrA_idx != 0)
+            answer[answer_idx++] = arrA[arrA_idx++];
+
+        while(arrB_len - arrB_idx != 0)
+            answer[answer_idx++] = arrB[arrB_idx++];
+        return answer;
+    }
+
+
+}
