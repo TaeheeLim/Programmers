27c07a813ef65720259dfa6bb081b6782e41d0bd
Taehee Lim <lim3617@naver.com>
2022-02-21 13:53:18 +0900

    in progress

--- main/java/com/example/codingtest/cospro/second/zero만들기.java
+++ main/java/com/example/codingtest/cospro/second/zero만들기.java
@@ -6,13 +6,14 @@ public class zero만들기 {
         System.out.println(solution(s));
     }
 
+    //"101101110"
     public static String solution(String s){
         s += '#';
         String answer = "";
         for(int i = 0; i < s.length(); ++i){
             if (s.charAt(i) == '0' && s.charAt(i+1) != '0')
                 answer += "0";
-            else
+            else if(s.charAt(i) == '1')
                 answer += "1";
         }
         return answer;
