23e910c4c8f54feed6416f6d88218773d7ca303a
Taehee Lim <lim3617@naver.com>
2022-02-24 12:34:01 +0900

    done: 소수만들기

--- main/java/com/example/codingtest/level1/소수만들기.java
+++ main/java/com/example/codingtest/level1/소수만들기.java
@@ -5,12 +5,34 @@ public class 소수만들기 {
         int[] numbers1 = {1,2,3,4};
         int[] numbers2 = {1,2,7,6,4};
 
-        System.out.println(solution(numbers1));
+        System.out.println(solution(numbers2));
     }
 
     public static int solution(int[] nums){
-        int answer = -1;
-        
+        int answer = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                for(int k = j + 1; k < nums.length; k++){
+                    int number = nums[i] + nums[j] + nums[k];
+                    if(check(number)){
+                        answer++;
+                    }
+                }
+            }
+        }
         return answer;
+    }
+
+    public static boolean check(int number){
+        boolean check = true;
+
+        for(int i = 2; i < number; i++){
+            if(number % i == 0 ){
+                check = false;
+                break;
+            }
+        }
+        return check;
     }
 }
