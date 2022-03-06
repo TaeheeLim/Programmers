4ba9d729ff318b0300452cf9093a9717839b6eb2
Taehee Lim <lim3617@naver.com>
2022-03-06 23:46:53 +0900

    done

--- main/java/com/example/codingtest/level1/문자열내마음대로정렬하기.java
+++ main/java/com/example/codingtest/level1/문자열내마음대로정렬하기.java
@@ -1,6 +1,6 @@
 package com.example.codingtest.level1;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class 문자열내마음대로정렬하기 {
 
@@ -10,15 +10,22 @@ public class 문자열내마음대로정렬하기 {
         int n1  = 1;
         int n2  = 2;
 
-        System.out.println(Arrays.toString(solution(strings1, n1)));
+        System.out.println(Arrays.toString(solution(strings2, n2)));
     }
 
     public static String[] solution(String[] strings, int n){
         String[] answer = {};
-        
+        List<String> list = new ArrayList<>();
 
+        for(int i = 0; i < strings.length; i++){
+            list.add(strings[i].charAt(n) + strings[i]);
+        }
+        Collections.sort(list);
 
-
+        for(String temporalValue : list){
+            answer = Arrays.copyOf(answer, answer.length + 1);
+            answer[answer.length - 1] = temporalValue.substring(1);
+        }
         return answer;
     }
 }
