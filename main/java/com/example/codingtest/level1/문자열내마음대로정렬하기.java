b79792910ff2bd2cbd1fc01bc897e5654100535b
Taehee Lim <lim3617@naver.com>
2022-03-06 23:18:08 +0900

    doing

---
+++ main/java/com/example/codingtest/level1/문자열내마음대로정렬하기.java
@@ -0,0 +1,24 @@
+package com.example.codingtest.level1;
+
+import java.util.Arrays;
+
+public class 문자열내마음대로정렬하기 {
+
+    public static void main(String[] args) {
+        String[] strings1 = {"sun", "bed", "car"};
+        String[] strings2 = {"abce", "abcd", "cdx"};
+        int n1  = 1;
+        int n2  = 2;
+
+        System.out.println(Arrays.toString(solution(strings1, n1)));
+    }
+
+    public static String[] solution(String[] strings, int n){
+        String[] answer = {};
+        
+
+
+
+        return answer;
+    }
+}
