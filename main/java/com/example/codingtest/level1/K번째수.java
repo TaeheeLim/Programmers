fddaaa0c654eeb973b956f60e0e84019e318f745
Taehee Lim <lim3617@naver.com>
2022-03-07 23:14:53 +0900

    k번째수

---
+++ main/java/com/example/codingtest/level1/K번째수.java
@@ -0,0 +1,25 @@
+package com.example.codingtest.level1;
+
+import java.util.Arrays;
+
+public class K번째수 {
+    public static void main(String[] args) {
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+
+        System.out.println(Arrays.toString(solution(array, commands)));
+    }
+
+    public static int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i = 0; i < commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] - 1];
+        }
+
+        return answer;
+    }
+}
