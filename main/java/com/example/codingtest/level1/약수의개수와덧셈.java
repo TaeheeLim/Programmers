bec49ae30672fd59dda7a0a920c0a3ae5bc94532
Taehee Lim <lim3617@naver.com>
2022-03-09 00:21:19 +0900

    done 약수의 개수와덧셈

--- main/java/com/example/codingtest/level1/약수의개수와덧셈.java
+++ main/java/com/example/codingtest/level1/약수의개수와덧셈.java
@@ -13,7 +13,25 @@ public class 약수의개수와덧셈 {
 
     public static int solution(int left, int right) {
         int answer = 0;
-        
+
+        for (int i = left; i <= right; i++) {
+            int count = getNumbers(i);
+
+            if(count % 2 == 0){
+                answer += i;
+            } else {
+                answer -= i;
+            }
+        }
+
         return answer;
+    }
+
+    public static int getNumbers(int number){
+        int check = 0;
+        for (int i = 1; i <= number; i++) {
+            if(number % i == 0) check++;
+        }
+        return check;
     }
 }
