bed5ebf34ce621582524ae4590e6077d354edd19
Taehee Lim <lim3617@naver.com>
2022-03-08 23:30:38 +0900

    add : 약수의개수와덧셈

---
+++ main/java/com/example/codingtest/level1/약수의개수와덧셈.java
@@ -0,0 +1,19 @@
+package com.example.codingtest.level1;
+
+public class 약수의개수와덧셈 {
+    public static void main(String[] args) {
+        int left1 = 13;
+        int left2 = 24;
+
+        int right1 = 17;
+        int right2 = 27;
+
+        System.out.println(solution(left1,right1));
+    }
+
+    public static int solution(int left, int right) {
+        int answer = 0;
+        
+        return answer;
+    }
+}
