3e9ce1f8a98c90bb2a48fd7a72f669d71a0edc15
Taehee Lim <lim3617@naver.com>
2022-03-10 16:54:52 +0900

    finished : 폰켓몬

--- main/java/com/example/codingtest/level1/폰켓몬.java
+++ main/java/com/example/codingtest/level1/폰켓몬.java
@@ -1,16 +1,39 @@
 package com.example.codingtest.level1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
 public class 폰켓몬 {
     public static void main(String[] args) {
         int[] num1 = {3,1,2,3};
         int[] num2 = {3,3,3,2,2,4};
         int[] num3 = {3,3,3,2,2,2};
+
+        System.out.println(solution(num3));
     }
 
     //N마리의 포켓몬 중 N / 2 의 포켓몬을 가질 수 있다.
     public static int solution(int[] nums) {
         int answer = 0;
+        int limit = nums.length / 2;
+        Stack<Integer> pocketBalls = new Stack<>();
+        Arrays.sort(nums);
 
-        return answer;
+        //1,2,3,3
+        //2,2,3,3,3,4
+        //2,2,2,3,3,3
+        for (int i = 0; i < nums.length; i++) {
+            if(!pocketBalls.isEmpty() && pocketBalls.peek() == nums[i]){
+                continue;
+            } else {
+                pocketBalls.push(nums[i]);
+            }
+        }
+        if(pocketBalls.size() <= limit){
+            limit = pocketBalls.size();
+        }
+        return limit;
     }
 }
